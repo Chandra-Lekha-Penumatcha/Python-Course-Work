bcaa28d5d6ba0eeb7bb2963a61f0b6a2ba1c880f
Chandra-Lekha-Penumatcha <chandralekhapenumatcha@gmail.com>
2026-09-05 17:49:18 +0530

    Day 30 - Constructor

---
+++ Day-30/Inheritance.py
@@ -0,0 +1,21 @@
+#single,multiple,multi level,hierachy,hybrid
+class whatsappv1:
+    def __init__(self,name):
+        self.name = name
+        print(f"Welcome to the whatsapp - v1 {self.name}!")
+    def messaging(self):
+        print("You can send messages")
+class whatsappV2(whatsappv1):
+    def __init__(self, name):
+       self.name = name
+       print(f"Welcome to the whatsapp - v2 {self.name}!")
+    def calls(self):
+        print("You can audio and video calls")
+
+chandra = whatsappv1('chandra')
+chandra.messaging()
+
+suhi = whatsappV2('suhi')
+suhi.messaging()
+suhi.calls()
+
---
+++ Day-30/constructor.py
@@ -0,0 +1,9 @@
+class Flipkart:
+    def __init__(self,name,phone):
+        self.name = name
+        self.phone = phone
+        print(f"Hello {self.name}, Welcome to the Flipkart")
+
+chandra = Flipkart('chandra',987654321)
+gayi = Flipkart('gayi',987654321)
+suhi = Flipkart('suhi',987654321)
