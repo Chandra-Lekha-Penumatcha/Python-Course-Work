37d187993fff56438dee0994ae07532bcccee563
Chandra-Lekha-Penumatcha <chandralekhapenumatcha@gmail.com>
2026-09-05 17:19:52 +0530

    Day 24 - ATM

---
+++ Day-24/caluclator/logic.py
@@ -0,0 +1,12 @@
+def add(a,b):
+    print("Addittion:",a+b)
+def sub(a,b):
+    print("Subtraction:",a-b)
+def mul(a,b):
+    print("Mult:",a*b)
+def div(a,b):
+    print("Div:",a/b)
+def exp(a,b):
+    print("Exp:",a**b)
+def remainder(a,b):
+    print("Rem:",a%b)
